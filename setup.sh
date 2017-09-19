2e0237c20a440285b086612b2adffd5845172dd2
Dirk Biesinger <dirk.biesinger@gmail.com>
2017-09-19 12:36:16 -0700

    Update setup.sh
    splitting setup.sh into two to allow path update take effect

--- setup.sh
+++ setup.sh
@@ -3,22 +3,4 @@ curl -O https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
 bash ./Anaconda3-4.4.0-Linux-x86_64.sh
 cd ~
 source ~/.bashrc
-mkdir certificate
-conda create --name kaggle_env python=3
-conda install --name kaggle_env numpy seaborn scipy scikit-learn keras tensorflow
-conda update conda
-conda update anaconda
-jupyter notebook --generate-config
-jupyter notebook password
-jupyter notebook --certfile=mycert.pem --keyfile mykey.key
-openssl req -x509 -nodes -days 3650 -newkey rsa:1024 -keyout mykey.key -out mycert.pem 
-echo "c.NotebookApp.certfile = u'/home/$USER/certificate/mycert.pem'" >> ./.jupyter_notebook_config.py
-echo "c.NotebookApp.keyfile = u'/home/$USER/certificate/mykey.key'" >> ./.jupyter_notebook_config.py
-echo "c.NotebookApp.ip = '*'" >> ./.jupyter_notebook_config.py
-echo "c.NotebookApp.open_browser = False" >> ./.jupyter_notebook_config.py
-echo "c.NotebookApp.port = 8890" >> ./.jupyter_notebook_config.py
-echo "c.NotebookApp.password_required = True" >> ./.jupyter_notebook_config.py
-pwd=$(cat ./.jupyter/jupyter_notebook_config.json | sed -n -e 's/^.*"password": //p' | sed 's/[^"]*"\([^"]*\)".*/\1/')
-echo "c.NotebookApp.password = u'$pwd'" >> ./.jupyter_notebook_config.py
-
-
+./notebook-server/setup2.sh
