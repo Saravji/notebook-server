a6cece5e40a1b3dc7e64993b3b4b69a1811a9fa8
Dirk Biesinger <dirk.biesinger@gmail.com>
2017-09-19 13:47:39 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ bash ./Anaconda3-4.4.0-Linux-x86_64.sh
 cd ~
 source ~/.bashrc
 mkdir certificate
+mkdir notebook_work
 #conda create --name kaggle_env python=3
 #conda install --name kaggle_env numpy seaborn scipy scikit-learn keras tensorflow
 conda install -y numpy seaborn scipy scikit-learn keras tensorflow
@@ -16,8 +17,9 @@ openssl req -x509 -nodes -days 3650 -newkey rsa:1024 -keyout ./certificate/mykey
 echo "c.NotebookApp.certfile = u'/home/$USER/certificate/mycert.pem'" >> ./.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.keyfile = u'/home/$USER/certificate/mykey.key'" >> ./.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.ip = '*'" >> ./.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.notebook_dir = u'/home/$USER/notebook_work' >> ./.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.open_browser = False" >> ./.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.port = 8890" >> ./.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.password_required = True" >> ./.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.password_required = False" >> ./.jupyter/jupyter_notebook_config.py
 pwd=$(cat ./.jupyter/jupyter_notebook_config.json | sed -n -e 's/^.*"password": //p' | sed 's/[^"]*"\([^"]*\)".*/\1/')
 echo "c.NotebookApp.password = u'$pwd'" >> ./.jupyter/jupyter_notebook_config.py
