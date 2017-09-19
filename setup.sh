16569eb4353ed198f719009c2e132af746f39357
Dirk Biesinger <dirk.biesinger@gmail.com>
2017-09-19 11:19:51 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 cd /tmp
 curl -O https://repo.continuum.io/archive/Anaconda3-4.4.0.1-Linux-x86_64.sh
-Anaconda3-4.4.0.1-Linux-x86_64.sh
+bash ./Anaconda3-4.4.0.1-Linux-x86_64.sh
 source ~/.bashrc
 cd ~
 mkdir certificate
